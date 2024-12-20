9542971a6553f7564820e02fc87ddb20bf58e90d
ltfranks <ltfranks@calpoly.edu>
2024-12-19 19:44:18 -0800

    Initial commit: clean start

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar"
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+}
+
+export default function Home() {
+  const {data: movies = []} = useMovieList();
+
+  return (
+    <>
+    <Navbar/>
+    <Billboard/>
+    <div className="pb-40">
+      <MovieList title="Trending Now" data={movies}/>
+    </div>
+    </>
+  );
+}
